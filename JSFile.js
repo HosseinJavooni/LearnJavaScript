fd331bec1fb54e3ff75a673f85f81f696245ee62
h.javooni <h.javooni@faraboom.co>
2023-09-25 16:28:27 +0330

    secondCommit

--- JSFile.js
+++ JSFile.js
@@ -2,11 +2,18 @@ console.log("Hello World")
 a = 5; b = 6; c = a + b;
 function doSome(){
     document.getElementById('test2').innerHTML = 7 + 9;
-    document.getElementById('test2').style.fontSize = "35px";
+    document.getElementById('test2').style.fontSize =
+    "35px";
     //document.write("<p> This is new </p>");
     window.alert("Do not do that!");
     alert("He can't go throug door when he come to Faraboom");
     //for send a html page to a printer we can use this function.
     window.print();
 }
+
+function deleteAnElement(){
+
+}
+// start from here:
+// https://www.w3schools.com/js/js_syntax.asp
 
